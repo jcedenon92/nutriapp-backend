69e78fc94ee82d5a38838a55bd9681102ebf20b9
Jorge Cedeño <jcedenon92@gmail.com>
2023-03-18 17:33:43 -0500

    Configuracion base

---
+++ src/main/java/com/jcedenon/NutriappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jcedenon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriappBackendApplication.class, args);
+    }
+
+}
